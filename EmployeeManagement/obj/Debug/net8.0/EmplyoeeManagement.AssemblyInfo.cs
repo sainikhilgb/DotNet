f008921b92c187f6a99eb3acd68b79bd524a537f
sainikhilgb <sainikhilgodl@gmail.com>
2024-11-18 13:28:17 +0530

    updated

--- EmployeeManagement/obj/Debug/net8.0/EmplyoeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net8.0/EmplyoeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmplyoeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47485040301dd65dcb4b165b5dbe12b994c81e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60ec15adc3b33897c753858dd1e439626911a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmplyoeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmplyoeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
